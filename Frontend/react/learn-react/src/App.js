02ce83ada4439620f8f8f365ef1a7fc263691eea
unknown <vs6838635@gmail.com>
2024-08-18 14:41:17 +0530

    form using useState

--- Frontend/react/learn-react/src/App.js
+++ Frontend/react/learn-react/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Counter />
+        <Form />
       </header>
     </div>
   );
--- Frontend/react/learn-react/src/components/form.js
+++ Frontend/react/learn-react/src/components/form.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Submitted:', formData);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+
   return (
-    <div>form</div>
+    <>
+    <div>Use of useState in form</div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <div>
+        <label>Name:</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange}  />
+        </div>
+        <div><label>email:</label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange}  /></div>
+        <div>
+        <label>Password:</label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange}  />
+        </div>
+        
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+    </>
   )
 }
 
